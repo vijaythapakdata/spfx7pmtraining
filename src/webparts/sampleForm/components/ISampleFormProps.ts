20ea2b84715927bbc3beb36bf101a60e403db0c8
vijaythapakdata <vijaythapak24@gmail.com>
2025-12-11 19:04:47 +0530

    latest

---
+++ src/webparts/sampleForm/components/ISampleFormProps.ts
@@ -0,0 +1,7 @@
+export interface ISampleFormProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/sampleForm/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISampleFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SampleFormWebPartStrings' {
+  const strings: ISampleFormWebPartStrings;
+  export = strings;
+}
